94b4a89f3e1426c2a826d08024126cd457a229d0
tetelab <tetelabtt@gmail.com>
2020-10-09 15:47:43 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 松岡哲平 on 2020/10/09.
+//  Copyright © 2020 tetelab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
